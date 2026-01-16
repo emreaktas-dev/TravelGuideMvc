c8229bbb50f65c898fdc5ae80b49c4f6ecc26d99
emreaktas <emreeaktas07@gmail.com>
2026-01-16 14:59:43 +0300

    chore: add initial MVC project files and EF Core setup

---
+++ TravelGuideMvc/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelGuideMvc.Models;
+
+namespace TravelGuideMvc.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Şimdilik boş - birazdan modelleri ekleyince buraya DbSet yazacağız.
+        // public DbSet<Place> Places => Set<Place>();
+    }
+}
