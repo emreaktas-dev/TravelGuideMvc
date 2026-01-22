ddfcc23aa8d8d019f452f512cb9d1ac6a1c637c2
emreaktas <emreeaktas07@gmail.com>
2026-01-22 23:19:43 +0300

    iyileştirmeler

--- TravelGuideMvc/Controllers/HomeController.cs
+++ TravelGuideMvc/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; // .Include() için gerekli
+using TravelGuideMvc.Data; // AppDbContext için gerekli (Data klasöründe olduðunu varsayýyorum)
 using TravelGuideMvc.Models;
+using TravelGuideMvc.ViewModels; // HomeVm için gerekli
 
 namespace TravelGuideMvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context; // Veritabaný servisi
 
-        public HomeController(ILogger<HomeController> logger)
+        // Constructor'a AppDbContext ekledik
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Veritabanýndan rastgele veya ilk 6 mekaný çekiyoruz.
+            // .Include ile Þehir ve Kategori bilgilerini de getiriyoruz ki kartlarda boþ çýkmasýn.
+            var featuredPlaces = _context.Places
+                                         .Include(x => x.City)
+                                         .Include(x => x.Category)
+                                         .OrderBy(x => x.Id) // Veya x.Name, isteðe baðlý sýralama
+                                         .Take(6) // Ana sayfada çok yýðýlma olmasýn diye sýnýrla
+                                         .ToList();
+
+            // ViewModel'i oluþturup veriyi içine koyuyoruz
+            var vm = new HomeVm
+            {
+                FeaturedPlaces = featuredPlaces
+            };
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ TravelGuideMvc/ViewModels/HomeVm.cs
@@ -0,0 +1,10 @@
+﻿using TravelGuideMvc.Models;
+
+namespace TravelGuideMvc.ViewModels
+{
+    public class HomeVm
+    {
+        // Ana sayfada göstereceğimiz öne çıkan mekanlar listesi
+        public List<Place> FeaturedPlaces { get; set; } = new List<Place>();
+    }
+}
